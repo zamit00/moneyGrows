be239a0214b5c62ab9e0770ddebde0982687cfa4
zamit00 <zamit00@gmail.com>
2025-01-30 23:52:38 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,216 @@
+async function filterMaslul(mas, moza){
+   var data;
+   
+        if (mas==='כללי'){
+            
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes(mas)
+            );
+            
+        data.sort((a, b) => b.tesuam - a.tesuam); 
+        return data;
+        }
+        
+        else if(mas==='עוקב מדד s&p 500'){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes('amp') &&
+                item.shemkupa.includes('500')
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="מניות"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes('מניות') &&
+                !item.shemkupa.includes('עוקב') &&
+                !item.shemkupa.includes('סחיר') &&
+                !item.shemkupa.includes('משולב') &&
+                !item.shemkupa.includes('25') &&
+                !item.shemkupa.includes('"אג\"ח"') && 
+                !item.shemkupa.includes('פאסיבי')
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="אשראי ואג\"ח"){
+                data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes('אשראי') &&
+                !item.shemkupa.includes('מניות') &&
+                !item.shemkupa.includes('עוקב') &&
+                !item.shemkupa.includes('סחיר') 
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="אשראי ואג\"ח עם מניות"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes('אשראי') &&
+                item.shemkupa.includes('25')  
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="כספי (שקלי)"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes('כספי (שקלי)') 
+                
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="עוקב מדדים - גמיש"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes('עוקב') &&
+                item.shemkupa.includes('גמיש')
+                
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="אג\"ח ממשלות"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes('ממשלות') 
+                
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="הלכה יהודית"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes('הלכה') 
+                
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="משולב סחיר"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes("משולב סחיר")   
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="עוקב מדדי אג\"ח"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes("עוקב") &&
+                item.shemkupa.includes("אג\"ח") &&
+                !item.shemkupa.includes("מניות")  
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="עוקב מדדי מניות"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes("מניות") &&
+                item.shemkupa.includes("עוקב")  
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="מניות סחיר"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes("מניות") &&
+                item.shemkupa.includes("סחיר")  
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+
+        }
+        else if(mas==="אג\"ח סחיר"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes("סחיר") &&
+                item.shemkupa.includes("אג\"ח")  
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+        }
+        else if(mas==="50-60"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes("50") &&
+                item.shemkupa.includes("60") 
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+
+        }
+        else if(mas==="עד 50"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes("50") &&
+                !item.shemkupa.includes("60") 
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+
+        }
+        else if(mas==="60 ומעלה"){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                !item.shemkupa.includes("50") &&
+                item.shemkupa.includes("60") 
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+
+        }
+        else if(mas==='סיכון מוגבר'){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes("מוגבר") 
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+         }
+         else if(mas==='סיכון מועט'){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes("מועט") 
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+         }
+         else if(mas==='סיכון בינוני'){
+            data = datanetunimKlaliX.filter(item => 
+                item.mozar === moza && 
+                item.tesuam !== undefined &&
+                item.shemkupa.includes("בינוני") 
+            );
+        data.sort((a, b) => b.tesuam - a.tesuam);    
+        return data;
+         }
+
+}
---
+++ scr.js
@@ -0,0 +1,100 @@
+
+window.onload = async function() {
+  await fetchdataJason();
+};
+async function fetchdataJason() {
+  try {
+      const response = await fetch('dataJason.json'); // שליפת הקובץ
+      if (!response.ok) {
+          throw new Error(`שגיאה: ${response.status} ${response.statusText}`);
+      }
+
+      const data = await response.json(); 
+      datanetunimKlaliX= data; 
+  } catch (error) {
+      console.error('שגיאה בשליפת הנתונים:', error);
+  }
+}
+function chng(x){
+   
+    x.classList.toggle("change");
+    var btn=document.getElementsByClassName('ser');
+    Array.from(btn).forEach(element=>{
+      if(element.className==="ser"){element.className="ser sera"}
+      else{element.className="ser"}
+     
+      
+  });
+  var allDropdowns = document.querySelectorAll('.dropdown-content');
+  allDropdowns.forEach(function(dropdown) {dropdown.style.display = "none";});
+  }
+
+  function taf(button) {
+   
+    var allDropdowns = document.querySelectorAll('.dropdown-content');
+    allDropdowns.forEach(function(dropdown) {
+      if (dropdown !== button.nextElementSibling) {
+        dropdown.style.display = "none";
+        
+      }
+    });
+  
+    var dropdownContent = button.nextElementSibling;
+    if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
+      
+      dropdownContent.style.display = "block";  
+    } else {
+      dropdownContent.style.display = "none";   
+    }
+    
+    
+  }
+
+
+
+  function hisht(x) {
+     
+
+    const screenw=window.innerWidth;
+    const screenh=window.innerHeight;
+    const maxw=Math.min(screenw*0.95,800);
+    const maxh=Math.min(screenh*0.95,600);
+    const windowf=`width=${maxw},height=${maxh},resizable=yes,scrollbars=yes`;
+    
+    // פתיחת הקובץ בחלון חדש
+    window.open( x, '_blank',  windowf
+    );
+  
+    // סגירת תפריט ההמבורגר
+    var hamburgerMenu = document.getElementById("tafrit");
+    if (hamburgerMenu.classList.contains("change")) {
+      hamburgerMenu.classList.remove("change");
+    }
+  
+    // החזרת הכפתורים למצבם הרגיל
+    var btns = document.getElementsByClassName('btn');
+    Array.from(btns).forEach(function(btn) {
+      if (btn.classList.contains("btna")) {
+        btn.classList.remove("btna");
+      }
+    });
+  
+    // הסתרת כל התפריטים הפתוחים
+    var allDropdowns = document.querySelectorAll('.dropdown-content');
+    allDropdowns.forEach(function(dropdown) {
+      dropdown.style.display = "none";
+    });
+  }
+  
+ 
+  const acceptBtn = document.getElementById('accept-btn');
+    acceptBtn.addEventListener('click', () => {
+      const overlay = document.getElementById('overlay');
+      const content = document.getElementById('content');
+      
+      const shimushbaatar = document.getElementById('shimushbaatar');
+        overlay.style.display = 'none'; 
+        content.style.display = 'block'; 
+        shimushbaatar.style.display = 'block';
+        maslulim(3);
+    });
