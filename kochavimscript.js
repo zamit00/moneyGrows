5a956f52bddc1b27a4a84658a13117acb7dc2008
zamit00 <zamit00@gmail.com>
2025-02-13 17:40:01 +0400

    Update kochavimscript.js

--- kochavimscript.js
+++ kochavimscript.js
@@ -85,8 +85,7 @@ async function tkofa(){
 }
 
 async function maslulim(t,moz){ 
- document.getElementById('kupaInfo').style.display='none';
-  document.getElementById("menu").classList.remove("open");
+ document.getElementById("menu").classList.remove("open");
   document.querySelector(".menu-btn").classList.remove("open");
   if (t===3){document.getElementById('filter').style.display='none';  
   document.querySelector('.filterChoose').style.display='none';}
