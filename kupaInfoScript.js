e37e620441df3652c9ad7eb3d05b3330d3958761
zamit00 <zamit00@gmail.com>
2025-02-13 17:25:45 +0400

    Add files via upload

---
+++ kupaInfoScript.js
@@ -0,0 +1,303 @@
+let datanetunimK;
+async function bring(data,mikom) {
+ 
+        const mhkupa = data[0].mh;
+        const muzar = data[0].mozar; 
+        const shemkupa = data[0].shemkupa;
+        const maslul = data[0].mas;
+        const tesuam = data[0].tesuam;
+        const tesuam36 = data[0].tesuam36;
+        const tesuam60 =data[0].tesuam60;
+        const menahelet = data[0].menahelet;
+        const yitratnechasim=data[0].yitratnechasim;
+        const stiya36=data[0].stiya36;
+        const stiya60=data[0].stiya60;
+
+        
+        
+ 
+    const mas = await maslultype(maslul);
+    document.getElementById('kupa').innerHTML+=shemkupa;
+    document.getElementById('sugmuzar').innerHTML='<span style="color: orangered;">'
+    +'סוג המוצר: '+ '</span>'+muzar; 
+    if(maslul!==undefined){  
+    document.getElementById('sugmaslul').innerHTML='<span style="color: orangered;">'
+    +'אפיק ההשקעה: '+ '</span>'+maslul
+    document.getElementById('ofi').innerHTML='<span style="color: orangered;">'
+    +'אופי מסלול ההשקעה: '+ '</span>'+mas[0]}
+    document.getElementById('mhkupa').innerHTML='<span style="color: orangered;">'
+    +'מספר קופה: '+ '</span>'+mhkupa
+    document.title =shemkupa+mhkupa;
+    let tesuaMitchilatshana=data[0].tesuaMitchilatshana;
+    let ramatsikon= data[0].ramatsikon; let shiurmenayut= data[0].kvutzaAhuz4751;
+
+    document.getElementById('stiya36').innerHTML='<span style="color: orangered;">'
+      +'סטיית תקן ל - 3 שנים: '+ '</span>'+stiya36;
+     
+
+  
+    var tchilatshana=1;
+    var yValues = [];
+    var xValues = [];
+    var numString; var year;var month;var formattedDate;
+
+       
+    for (let r = 1; r <= 12; r ++) {
+        const kvutza=`tesua${r}`
+        let parts = data[0][kvutza].split("=");
+        yValues.push(Number(parts[0]));
+        numString = parts[1].toString();
+        year = numString.substring(0, 4);
+        month = numString.substring(4, 6);
+        formattedDate = month + '/' + year;
+        xValues.push(formattedDate);
+
+
+    }
+
+
+    document.getElementById("miztaberet").innerHTML ='<span style="color: orangered;">'
+    + 'תשואה מצטברת מתחילת שנה: '+ '</span>'+data[0].tesuaMitchilatshana+"%"
+    document.getElementById("shana").innerHTML ='<span style="color: orangered;">'
+    + 'תשואה מצטברת לשנה: '+ '</span>'+tesuam+"%";
+    document.getElementById("shalosh").innerHTML ='<span style="color: orangered;">'
+      + 'תשואה מצטברת ל -3 שנים: '+ '</span>'+tesuam36+"%";
+      if(maslul!==undefined){
+    document.getElementById("derog").innerHTML ='<span style="color: orangered;">'
+    + 'דירוג: '+ '</span>'+
+  'הקופה מדורגת במקום ה - <span style="color: green;font-size:22px">' + mikom + '</span>' + 
+  ' באפיק ההשקעה ' + maslul + '.';}
+  else{
+    document.getElementById("derog").innerHTML ='<span style="color: orangered;">'
+    + 'דירוג: '+ '</span>'+
+  'הקופה מדורגת במקום ה - <span style="color: green;font-size:22px">' + mikom + '</span>' + 
+  ' באפיק ההשקעה '  + '.';
+  }
+
+    var barColors = yValues.map(function (value) {
+        return value >= 0 ? "green" : "red"; // Green for positive values, red for negative values
+    });
+
+    new Chart("myChartkupa", {
+        type: "bar",
+        data: {
+            labels: xValues,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yValues
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: "תשואות 12 חודשים אחרונים"
+                    ,font: {
+                size: 20,  
+                family: 'Arial',  // Optional: set the font family
+                weight: 'bold'  // Optional: set the font weight
+            },
+            color: 'blue'  // Set the text color
+                },
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    min: Math.min(0, Math.min(...yValues))
+                },
+                x: {
+                    ticks: {
+                        autoSkip: false
+                    }
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+   
+       
+    
+    document.getElementById('ramatsikon').innerHTML='<span style="color: orangered;">'
+                +'רמת סיכון: '+ '</span>'+ramatsikon +" ,חשיפה למניות - "+shiurmenayut+"%"
+    var nehasim=[];
+    for (let i = 4701; i <= 4710; i++) {
+      const keySchum = `kvutzaSchum${i}`;
+      const keyAhuz = `kvutzaAhuz${i}`;
+      const keySug = `kvutzaSug${i}`;
+    
+    if (data[0][keySchum]>0) {
+        nehasim.push(data[0][keySug]);
+        nehasim.push(data[0][keySchum]);
+        nehasim.push(data[0][keyAhuz]);
+    }
+}
+       
+       pie(nehasim);    
+                       
+   
+}
+
+
+
+
+// פונקציות tesua ו-maslultype ללא שינוי
+async function tesua(y) {
+    const x = y.toString();
+
+    return fetch('kupotHodshAharon.xml')
+        .then(response => response.text())
+        .then(xmlString => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(xmlString, "application/xml");
+            const rows = xmlDoc.getElementsByTagName("Row");
+
+            var rowsForIDKupa = Array.from(rows).filter(row => {
+                return row.getElementsByTagName("ID_KUPA")[0].textContent === x;
+            });
+
+            const datareturn = [];
+            var yValue, xValue;
+            for (let i = 0; i < rowsForIDKupa.length; i++) {
+                yValue = rowsForIDKupa[i].getElementsByTagName("TSUA_NOMINALI_BFOAL")[0].textContent;
+                xValue = rowsForIDKupa[i].getElementsByTagName("TKF_DIVUACH")[0]?.textContent || "default";
+
+                datareturn.push(parseFloat(Number(yValue)));
+                datareturn.push(parseFloat(Number(xValue)));
+            }
+           return datareturn
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            return [];
+        });
+}
+
+
+
+async function maslultype(y) {
+    try {
+        const response = await fetch('ofihashkaa.xml');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const xmlString = await response.text();
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlString, "application/xml");
+
+        const rows = xmlDoc.getElementsByTagName("Row");
+        var databack=[];
+        for (let i = 0; i < rows.length; i++) {
+            const row = rows[i];
+            const mhkupa = row.getElementsByTagName("ID")[0]?.textContent || '';
+            const shemkupa = row.getElementsByTagName("SHM_KUPA")[0]?.textContent || '';
+            const sikon = row.getElementsByTagName("SIKON")[0]?.textContent || '';
+            if (mhkupa === y) {
+                databack.push(shemkupa);
+                databack.push(sikon)
+                return databack;
+            }
+        }
+
+        return [];
+    } catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
+}
+
+function pie(nehasim){
+  var tda;var trma;
+        const tbl = document.getElementById('nehasim');   
+        document.getElementById('nehasimkot').innerText='חלוקת נכסים לקבוצות ראשיות:';  
+              
+        var shemhaneches=[];var ahuzhaneches=[];
+        for(let i=0;i<nehasim.length;i+=3){
+            
+              trma = document.createElement('tr');
+              trma.className='trkupa'
+              tda = document.createElement('td');
+              tda.className="tdmhkupa";
+              tda.innerHTML = `<span style="font-weight: bold; color: blue;">${nehasim[i]}:</span> 
+                 ${Number(nehasim[i+1]).toLocaleString()} אש"ח 
+                 , שיעור מהנכסים ${Number(nehasim[i+2]).toFixed(2)}%`;              shemhaneches.push(nehasim[i]);
+              ahuzhaneches.push(Number(nehasim[i+2]));
+              trma.appendChild(tda);
+             
+              tbl.appendChild(trma);
+              
+            }
+                     
+  const labels = shemhaneches;
+  const dataValues = ahuzhaneches;
+  const backgroundColors = [
+    "#FF6384", "#36A2EB", "#FFCE56", "#4BC0C0", 
+    "#9966FF", "#FF9F40", "#C9CBCF", "#8B0000","#FFD700"   
+];
+
+new Chart("pieChartkupa", {
+    type: "pie",
+    data: {
+        labels: labels,
+        datasets: [{
+            data: dataValues,
+            backgroundColor: backgroundColors
+        }]
+    },
+    options: {
+      responsive: true, // הופך את הקנבס לרספונסיבי
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                
+                font: {
+                    size: 20,
+                    family: "Arial",
+                    weight: "bold"
+                },
+                color: "#333"
+            },
+            legend: {
+                display: true,
+                position: "bottom", // Can be 'top', 'bottom', 'left', 'right'
+                 align: "end"
+              }
+        },
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
+const canvas = document.getElementById("pieChartkupa");
+document.body.appendChild(canvas);
+}
+
+
+function exportToPDF() {
+    if(document.getElementById('pdflink')){return;}
+    const element = document.getElementById('kupaInfo');
+
+    html2pdf().from(element).toPdf().get('pdf').then(function(pdf) {
+        const blob = pdf.output('blob');
+        const fileURL = URL.createObjectURL(blob);
+
+        // יצירת לינק דינמי
+        const link = document.createElement('a');
+        link.href = fileURL;
+        link.id='pdflink';
+        link.download = 'document.pdf';
+        link.innerText = 'לחץ כאן להורדת PDF';
+        
+        // הוספת הלינק לעמוד
+        document.body.appendChild(link);
+
+        // הצגת הלינק (או ניתן לשתף ב-WhatsApp)
+        console.log('קובץ PDF נוצר:', fileURL);
+    });
+}
+    
+    
