04a3d3fae42a5e48617ccc89c29db28619c62642
zamit00 <zamit00@gmail.com>
2025-02-22 08:43:47 +0200

    Update dmeyNihulScript.js

--- dmeyNihulScript.js
+++ dmeyNihulScript.js
@@ -125,16 +125,16 @@ function hashev(x) {
     
     const rb=parseFloat(x);        
     // Retrieve values from input fields
-    const hp=parseFloat(document.getElementById("hadpeami").replace(/[^\d]/g, ''));	
-    const tash= parseFloat(document.getElementById("hodshi").replace(/[^\d]/g, ''));
+    const hp=document.getElementById("hadpeami");	
+    const tash= document.getElementById("hodshi");
     const dmnz1=parseFloat(document.getElementById('dmnz1').value);
     const dmnz2=parseFloat(document.getElementById('dmnz2').value);
     const dmnh1=parseFloat(document.getElementById('dmnh1').value);
     const dmnh2=parseFloat(document.getElementById('dmnh2').value);
     
-    let x1 = parseInt(hp.value);
+    let x1 = parseFloat(hp.value);
     if(!x1){x1=0};
-    let x2 = parseInt(tash.value);
+    let x2 = parseFloat(tash.value);
     if(!x2){x2=0};
     if(x1===0 && x2===0){alert('הוסף סכומי השקעה')
         var element=document.getElementById('dropdown-schom')
