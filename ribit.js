2625135e650a268cc1ef0152a5be56fd279d63f5
zamit00 <zamit00@gmail.com>
2025-02-28 07:55:12 +0200

    Update ribit.js

--- ribit.js
+++ ribit.js
@@ -1,107 +1,137 @@
-const select=document.getElementById('txttkofa1');
-const dmn=document.getElementById('txttkofa2');
-const otherribit=document.getElementById('selecttoz');
-for (let i = 1; i <= 30; i++) {
-    let option = document.createElement('option');
-    option.value = i;  
-    option.textContent = i;  
-    if(i===10){option.selected = true;}
-    select.appendChild(option);  
-}
-
-
-for (let i = 0; i <= 1.1; i += 0.05) {
-    let option = document.createElement('option');
-    
-    // Check if i is approximately 1, with a tolerance
-    if (Math.abs(i - 0.5) < 0.0001) {
-        option.selected = true;
-    }
-    
-    let value = Math.round(i * 100) / 100;  
-    option.value = value;  
-    option.textContent = value+"%"; 
-    dmn.appendChild(option);  
-}
-
-for (let i = 0.01; i <= 0.21; i += 0.01) {
-    let option = document.createElement('option');
-    
-    // Check if i is approximately 1, with a tolerance
-    if (Math.abs(i - 0.04) < 0.0001) {
-        option.selected = true;
-    }
-    
-    let value = Math.round(i * 100) / 100;  
-    option.value = value;  
-    option.textContent = Math.round(value*100)+"%"; 
-    otherribit.appendChild(option);  
-} 
- 
-
-      
-
 function onch(){
     const tablediv= document.getElementById("tozaot");
-    tablediv.innerHTML=""; 
-    document.getElementById("alltoz").style.display="none";
+       tablediv.innerHTML=""; 
+       document.getElementById("alltoz").style.display="none";
+       document.getElementById('dropdown-sug').style.display='flex' ;
+       document.getElementById('dropdown-tkofa').style.display='flex' ;
+       document.getElementById('dropdown-schom').style.display='flex' ;
 }
 
+function opn(x){
+    var element=document.getElementById(x)
+    if(element.style.display==='none' || element.style.display==='' ){
+        element.style.display='flex'
+        if(x==='dropdown-schom'){rdchange()}
+        if(x==='dropdown-tkofa'){addelement()}
+    
+    }
+    else{element.style.display='none'}
 
+}
 function rdchange(){
    
-    var rd1=document.getElementById('rdbutton1');
-    var rd2=document.getElementById('rdbutton2');
-    var rd3=document.getElementById('rdbutton3');
-    if(rd1.checked){
-        document.getElementById('schomdiv1').style.display='flex';
-        document.getElementById('schomdiv2').style.display='none';
-        document.getElementById('hodshi').innerText=100;
-    }
-    else if(rd2.checked){
-        document.getElementById('schomdiv1').style.display='none';
-        document.getElementById('schomdiv2').style.display='flex';
-        document.getElementById('hadpeami').innerText=100;
-    }
-    else if (rd3.checked){
-        document.getElementById('schomdiv1').style.display='flex';
-        document.getElementById('schomdiv2').style.display='flex';
-    }
+        var rd1=document.getElementById('rdbutton1');
+        var rd2=document.getElementById('rdbutton2');
+        var rd3=document.getElementById('rdbutton3');
+        if(rd1.checked){
+            document.getElementById('schomdiv1').style.display='block';
+            document.getElementById('schomdiv2').style.display='none';
+            document.getElementById('hodshi').innerText=100;
+        }
+        else if(rd2.checked){
+            document.getElementById('schomdiv1').style.display='none';
+            document.getElementById('schomdiv2').style.display='block';
+            document.getElementById('hadpeami').innerText=100;
+        }
+        else if (rd3.checked){
+            document.getElementById('schomdiv1').style.display='block';
+            document.getElementById('schomdiv2').style.display='block';
+        }
     
 }
 
-function othribit(){
-    var rb=document.getElementById('selecttoz').value;
-    document.getElementById("kottoz").textContent = `לפי ריבית ${Math.round(rb*100)}% שנתי:`;       
+    const select=document.getElementById('txttkofa1');
+    const dmn=document.getElementById('txttkofa2');
+    const otherribit=document.getElementById('selecttoz');
+    for (let i = 1; i <= 30; i++) {
+        let option = document.createElement('option');
+        option.value = i;  
+        option.textContent = i;  
+        if(i===10){option.selected = true;}
+        select.appendChild(option);  
+    }   
+   
+      
+    for (let i = 0; i <= 1.1; i += 0.05) {
+        let option = document.createElement('option');
+        
+        // Check if i is approximately 1, with a tolerance
+        if (Math.abs(i - 1) < 0.0001) {
+            option.selected = true;
+        }
+        
+        let value = Math.round(i * 100) / 100;  
+        option.value = value;  
+        option.textContent = value+"%"; 
+        dmn.appendChild(option);  
+    }
+    for (let i = 0.01; i <= 0.21; i += 0.01) {
+        let option = document.createElement('option');
+        
+        // Check if i is approximately 1, with a tolerance
+        if (Math.abs(i - 0.04) < 0.0001) {
+            option.selected = true;
+        }
+        
+        let value = Math.round(i * 100) / 100;  
+        option.value = value;  
+        option.textContent = Math.round(value*100)+"%"; 
+        otherribit.appendChild(option);  
+    }
+    function othribit(){
+        var rb=document.getElementById('selecttoz').value;
+
+// עדכון הטקסט בתוך האלמנט עם ID kottoz
+document.getElementById("kottoz").textContent = `לפי ריבית ${Math.round(rb*100)}% שנתי:`;       
         hashev(rb);
     }
 
 function hashev(x) {
-    const rb=parseFloat(x);     
+    
+    const rb=parseFloat(x);        
     // Retrieve values from input fields
     const hp=document.getElementById("hadpeami");
     const tash=document.getElementById("hodshi");
-    const rdbutton1=document.getElementById("rdbutton1");
-    const rdbutton2=document.getElementById("rdbutton2");
-    const rdbutton3=document.getElementById("rdbutton3");
-    let x1 = parseInt(hp.value);
+    let x1 = parseInt(hp.value.replace(/,/g, ""));
     if(!x1){x1=0};
-    let x2 = parseInt(tash.value);
+    let x2 = parseInt(tash.value.replace(/,/g, ""));
     if(!x2){x2=0};
-     
-    if(x1===0 && x2===0){alert('הוסף סכומי השקעה');return;}
-    if(rdbutton1.checked && x1===0){alert('הוסף סכום השקעה חד פעמי');return;}
-    if(rdbutton2.checked && x2===0){alert('הוסף סכום השקעה חודשי');return;}
-    if(rdbutton3.checked && (x1===0 || x2===0)){alert('הוסף סכומי השקעה');return;}
+    if(x1===0 && x2===0){alert('הוסף סכומי השקעה')
+        var element=document.getElementById('dropdown-schom')
+        element.style.display='flex' ;               
+        
+        var rd1=document.getElementById('rdbutton1');
+        var rd2=document.getElementById('rdbutton2');
+        var rd3=document.getElementById('rdbutton3');
+        if(rd1.checked){
+            document.getElementById('schomdiv1').style.display='block';
+            document.getElementById('schomdiv2').style.display='none';
+            document.getElementById('hodshi').innerText=100;
+        }
+        else if(rd2.checked){
+            document.getElementById('schomdiv1').style.display='none';
+            document.getElementById('schomdiv2').style.display='block';
+            document.getElementById('hadpeami').innerText=100;
+        }
+        else if (rd3.checked){
+            document.getElementById('schomdiv1').style.display='block';
+            document.getElementById('schomdiv2').style.display='block';
+        }        
+               
+        return;}
+    let dn =document.getElementById("txttkofa2").value;
+    /*let simul=document.getElementById('selectRibit');*/
+    
+    //let ribitchoose=document.getElementById("selectribit");
+    //let ribit=ribitchoose.value;
+    if(!dn || dn===0){dn=1;}
+    let z=rb-dn/100;
 
     if(x1<0||x2<0){alert('סכומים לא תקינים');return;}
-    let dn =document.getElementById("txttkofa2").value;
-    if(!dn || dn===0){dn=1;}           
-    
-    let z=rb-dn/100;
-    
     let selectElement = document.getElementById("txttkofa1");
+    if(!selectElement.value || selectElement.value===0){addelement(); }
     let x3 = selectElement.value; 
+    
     
    const textopen="סכום השקעה עתידי לתקופה של - ";
    const textshanim=" שנים:       ";
@@ -114,6 +144,11 @@ function hashev(x) {
    sum30=hishuv(x1,x2,z,30);
  
     if (isNaN(sum) ) {alert('סכומים לא תקינים');return;}
+        
+      /* console.log(sum.toLocaleString());
+       console.log(sum10.toLocaleString());
+       console.log(sum20.toLocaleString());
+       console.log(sum30.toLocaleString());*/
 
        var table; var td; var tr;let par;let selectsim;
        const tablediv= document.getElementById("tozaot");
@@ -146,6 +181,7 @@ function hashev(x) {
                 td.className="txttd";
                 tr.appendChild(td);
                 td=document.createElement("td");
+                console.log(x2 + ","+x3)
                 td.innerText=(x2*x3*12).toLocaleString() + shach;
                 td.className="numtd";
                 tr.appendChild(td);
@@ -212,7 +248,12 @@ function hashev(x) {
             td.className="numtd";
             tr.appendChild(td);
             }
-            
+            var element=document.getElementById('dropdown-tkofa');
+            element.style.display='flex';
+             var elementa=document.getElementById('dropdown-schom');
+            elementa.style.display='none';
+            var elementb=document.getElementById('dropdown-sug');
+            elementb.style.display='none';
         
         function hishuv(x,y,z,t){
             let hishuv = x + y;
@@ -224,3 +265,4 @@ function hashev(x) {
             return hishuv;
         }
     }
+
